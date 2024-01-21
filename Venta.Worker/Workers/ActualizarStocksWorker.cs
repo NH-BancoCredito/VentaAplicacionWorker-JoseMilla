e183cd80a3bb1b163f68b11bd023a6dcb290bf98
jose milla <josmilla@gmail.com>
2024-01-21 13:19:09 -0500

    worked stocks

---
+++ Venta.Worker/Workers/ActualizarStocksWorker.cs
@@ -0,0 +1,33 @@
+﻿
+using Stocks.Domain.Service.Events;
+using static Confluent.Kafka.ConfigPropertyNames;
+using System.Threading;
+
+namespace Venta.Worker.Workers
+{
+    public class ActualizarStocksWorker : BackgroundService
+    {
+        private readonly IConsumerFactory _consumerFactory;
+        public ActualizarStocksWorker(IConsumerFactory consumerFactory) {
+            _consumerFactory = consumerFactory;
+        }
+        protected override Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var consumer = _consumerFactory.GetConsumer();
+            consumer.Subscribe("stocks");
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var consumeResult = consumer.Consume(cancellationToken);
+                var data = 1;
+
+                // handle consumed message.
+                
+            }
+
+            consumer.Close();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ VentaWorker.Infrastructure/Services/Events/ConsumerFactory.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+using Stocks.Domain.Service.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocks.Infrastructure.Services.Events
+{
+    public class ConsumerFactory : IConsumerFactory
+    {
+        private static Lazy<IConsumer<string, string>> LazyKafkaConnection = null;
+
+        public ConsumerFactory(ConsumerConfig config)
+        {
+            LazyKafkaConnection = new Lazy<IConsumer<string, string>>(() => new ConsumerBuilder<string, string>(config).Build());
+        }
+
+        public IConsumer<string, string> GetConsumer()
+            => LazyKafkaConnection.Value;
+
+       
+    }
+}
